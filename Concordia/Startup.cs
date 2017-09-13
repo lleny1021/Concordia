948b39f9ff81a0fc660805af4a5633aed7571fcd
lleny <lleny1021@hotmail.com>
2017-09-13 06:52:15 -0500

    Creación del Proyecto

---
+++ Concordia/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Concordia.Startup))]
+namespace Concordia
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
